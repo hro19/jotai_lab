ad068903fef38f2f56168d8a7826ba2455b8d027
hro19 <fourbasic19@gmail.com>
2024-03-28 18:35:56 +0900

    findIdCircleを使用する、errorの時の挙動を見る

--- src/pages/Kadai1.tsx
+++ src/pages/Kadai1.tsx
@@ -1,24 +1,36 @@
 import NavWrap from "../components/NavWrap";
-import { Center } from "@yamada-ui/react";
+import { Box } from "@yamada-ui/react";
 import { useAtom } from "jotai";
 import { countAtom,doubledAtom } from "../store/countAtoms";
+import { findIdCircle } from "../features/findIdCircle";
+import { circleType } from "../types/circle";
 
 const Kadai1 = () => {
   const [count] = useAtom(countAtom);
   const [doubledCount] = useAtom(doubledAtom);
+  const circles:circleType[]  = [
+    { circleId: 1, x: 10, y: 20, figure: "circle" },
+    { circleId: 2, x: 30, y: 40, figure: "circle" },
+    { circleId: 3, x: 50, y: 60, figure: "rect" },
+    { circleId: 4, x: 70, y: 80, figure: "circle" },
+];
+ const purposeCircle = findIdCircle(circles, 4);
+//  console.log(purposeCircle);
+// errorが投げられたときは画面が表示されない
+//  const purposeCircle2 = findIdCircle(circles, 5);
 
   return (
     <>
       <NavWrap pageTitle="課題1" />
-      <Center
+      <Box as={"div"}
         width={600}
         height={400}
         mx={"auto"}>
-        {count.toString()}
-        <br />
-        {doubledCount.toString()}
-        <br />
-      </Center>
+        <p>{count.toString()}</p>
+        <p>{doubledCount.toString()}</p>
+        <p>{purposeCircle.circleId}</p>
+        {/* <p>{purposeCircle2.circleId}</p> */}
+      </Box>
     </>
   );
 };
