236de4840c298f5969255c82f98f726cf1805527
hro19 <fourbasic19@gmail.com>
2024-03-28 17:32:44 +0900

    数値を足し合わせる関数にjsDocを追加

--- src/features/add.ts
+++ src/features/add.ts
@@ -1,3 +1,9 @@
-export function add(a: number, b: number): number {
-    return a + b;
+/**
+ * 2つの数値を足し合わせて結果を返します。
+ * @param num1 足し合わせる最初の数値。
+ * @param num2 足し合わせる2番目の数値。
+ * @returns 足し合わせた結果の数値。
+ */
+export function add(num1: number, num2: number): number {
+    return num1 + num2;
 }
