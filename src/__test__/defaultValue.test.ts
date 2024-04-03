bf4027a5e4435957d53c5dd967b10a43b90d3ad9
hro19 <fourbasic19@gmail.com>
2024-04-03 09:30:22 +0900

    defaultValueのテストと関数を追加

---
+++ src/__test__/defaultValue.test.ts
@@ -0,0 +1,13 @@
+import { culculateAddTax } from '../features/defaultValue';
+
+describe('culculateAddTax', () => {
+    it('should calculate the sum with tax', () => {
+        const result = culculateAddTax({ sum: 100, tax: 0.3 });
+        expect(result).toBe(130);
+    });
+
+    it('should calculate the sum without tax', () => {
+        const result = culculateAddTax({ sum: 100 });
+        expect(result).toBe(110);
+    });
+});
---
+++ src/features/defaultValue.ts
@@ -0,0 +1,2 @@
+export const culculateAddTax = ({sum, tax = 0.1}:{sum:number,tax?:number}) => sum + sum * tax;
+
